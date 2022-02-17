3e6e042c3cb3b81ba39c683a211aedde9a566210
reshminairlr <97173378+reshminairlr@users.noreply.github.com>
2022-02-17 17:02:06 +0530

    cmt

--- checklist.js
+++ checklist.js
@@ -8,25 +8,73 @@ if(this.readyState==4&&this.status==200)
    
 var response=JSON.parse(this.responseText);
 // console.log(response[0].title);
-var checklist="",compltd="";
-
+// var checklist="",compltd="";
+console.log("length="+response.length);
 for(var j=0;j<response.length;j++)
 {
-    checklist+=response[j].title;
-// console.log(checklist);
-var checkbox = document.createElement('input');
+//     checklist+=response[j].title;
+  console.log(response[j].title);
+// console.log("checklist="+checklist);
+
+// console.log("completed"+response[j].completed);
+if(response[j].completed==true)
+{
+  var checkbox = document.createElement('input');
+checkbox.type = "checkbox";
+checkbox.name = "name";
+checkbox.value = "value";
+checkbox.id = "id";
+checkbox.checked="true";
+var label = document.createElement('label')
+label.htmlFor = "id";
+var linebreak = document.createElement("br"); 
+// var space = document.createElement("&nbsp;");
+// label.style.whiteSpace = "pre-wrap";
+
+label.appendChild(document.createTextNode(response[j].title));
+// label.style.whiteSpace = "pre-wrap";
+check_box.appendChild(checkbox);
+// check_box.appendChild(space);
+check_box.appendChild(label);
+check_box.appendChild(linebreak);
+
+
+}
+
+
+else{
+    var checkbox = document.createElement('input');
 checkbox.type = "checkbox";
 checkbox.name = "name";
 checkbox.value = "value";
 checkbox.id = "id";
 var label = document.createElement('label')
 label.htmlFor = "id";
-label.appendChild(document.createTextNode(checklist));
-console.log("label"+label);
-console.log("checkbox"+checkbox);
+var linebreak = document.createElement("br");
+// var space = document.createElement("&nbsp;");
 
-check_list.appendChild(checkbox);
-check_list.appendChild(label);
+// label.style.whiteSpace = "pre-wrap";
+label.appendChild(document.createTextNode(response[j].title));
+// label.style.whiteSpace = "pre-wrap";
+check_box.appendChild(checkbox);
+// check_box.appendChild(space);
+
+check_box.appendChild(label);
+check_box.appendChild(linebreak);
+
+
+
+
+}
+
+// else{
+//     label.appendChild(document.createTextNode(checklist));
+
+// }
+// console.log("label"+label);
+// console.log("checkbox"+checkbox);
+
+
 
 
 
